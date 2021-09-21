f5d916d9c179d2f4cb4bc9291e7d7b14cd86750e
Prashant <prashantkumarjha47gmail.com>
2021-09-21 22:43:55 +0530

    first commit

---
+++ src/store/images.js
@@ -0,0 +1,7 @@
+export const images = {
+    kia: require("../assets/images/Kia.jpeg").default,
+    Hundai: require("../assets/images/Hundai_Venue.jpeg").default,
+    Ford: require("../assets/images/Ford-EcoSport-Right-Front-Three-Quarter-159249.jpeg").default,
+    car: "https://stimg.cardekho.com/images/car-images/large/Tata/Nexon/7297/1579769288048/224_Flame-Red_a52411.jpg",
+    no_data: require("../assets/images/no-data-icon-10.jpeg").default
+};
